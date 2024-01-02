20ac90a9d542d598e19edc3ae62491e8d5b02347
Helmut Qualtinger <94598493+HelmutQualtinger@users.noreply.github.com>
2024-01-02 12:18:51 +0100

    Better explanation also in English, add pdf plot

--- README.md
+++ README.md
@@ -2,6 +2,20 @@
 
 Ich werte die Daten meiner Kinomap Trainingssession aus. Kinomap sendet am Ende der Session unter anderem eine CSV Datei per E-mail an mich.
 
+Die Beispielsdatei zeigt einen Stufentest, bei dem ich ich anfänglich für 2 Minuten 50 Watt im Wattprogram des Ergometers trat und alle 2 Minuten um 25 watt erhöhte bis auf 250 W. Anschliessend machte ich einen Cooldown bei dem ich alle 2 Minuten die Leistung wieder um 50 W senkte.
+
+Das sieht man im linken Plot. Im rechten Plot sieht man die Korrelation zwischen Leistung und Herzpuls, Man sieht eine Hysteräse. Beim Cooldown ging der Puls nicht so schnell runter, wie er beim Belastungsanstieg gestiegen war. Die Stufen spiegeln den stufenweisen Anstieg der Leistung wieder. Der Puls steigt kontinuierlich. Er wurde über 30 Sekunden geglättet.
+
+
+
+# Project
+
+I analyze the data from my Kinomap training session. Kinomap sends me a CSV file via email at the end of the session.
+
+The example file shows a step test, where initially I cycled at 50 watts for 2 minutes in the watt program of the ergometer and increased by 25 watts every 2 minutes until reaching 250 watts. Then I did a cooldown where I decreased the power by 50 watts every 2 minutes.
+
+You can see that in the left plot. In the right plot, you can see the correlation between power and heart rate. There is a hysteresis effect. During the cooldown, the heart rate did not decrease as quickly as it increased during the load increase. The steps reflect the gradual increase in power. The heart rate increases continuously. It was smoothed over 30 seconds.
+
 
 
 
--- kinomap.py
+++ kinomap.py
@@ -1,39 +1,41 @@
 
 import pandas as pd
 import numpy as np
-import datetime
-import pandas as pd
 import matplotlib.pyplot as plt
+
 
 race = pd.read_csv("race.csv")
 
+# drop location columns, I'm not really moving
 race.drop(['Latitude', 'Longitude', 'Altitude'], axis=1, inplace=True)
-race = race[race['Heart rate'] > 50]
 
+# drop columns with no data for heart rate
+race = race[race['Heart rate'] > 50]
+# extract heart rate column as array so as to smooth it
 heart_rat_array = np.array(race['Heart rate'])
-print(len(heart_rat_array))
 smooth_heart_rat_array = np.zeros(len(heart_rat_array))
 for t in range(15, len(smooth_heart_rat_array)-15):
     for i in range(-15, 15):
         smooth_heart_rat_array[t] += heart_rat_array[t+i]
     smooth_heart_rat_array[t] /= 30
 
+# add smooted heart rate column to dataframe
 race["Smooth heart rate"] = smooth_heart_rat_array
 race = race[race['Smooth heart rate'] > 75]
 
 # Convert Date column to time in seconds
 race['Seconds'] = pd.to_datetime(race['Date']).astype(np.int64) // 10**9
 
-# Calculate the duration in seconds of the first row
+# Calculate the duration in minutes since the start of the session
 race['Duration'] = (race['Seconds'] - race['Seconds'].iloc[0])/60
 
-# Plot Heart rate vs Duration
+# Plot Heart rate and power vs Duration with independent y axis
 fig1, (ax1, ax3) = plt.subplots(1, 2, figsize=(12, 7))
 ax1.plot(race['Duration'], race['Smooth heart rate'],
          color='red', linewidth=3, label='Heart rate')
-ax1.set_xlabel('Duration')
-ax1.set_ylabel('Heart rate')
-ax1.set_title('Heart rate and  vs Duration')
+ax1.set_xlabel('Duration(min)')
+ax1.set_ylabel('Heart rate(bpm )', color='red')
+ax1.set_title('Heart rate and Power vs Duration')
 heart_interval = np.arange(70, 135, 5)
 ax1.set_yticks(list(range(75, 130, 5)))
 ax1.legend(shadow=True, fontsize='x-large', loc='upper left')
@@ -44,7 +46,7 @@ ax2 = ax1.twinx()
 # Plot Power vs Duration
 ax2.plot(race['Duration'], race['Power'], color='blue', label='Power')
 ax2.set_ylabel('Power', color='blue')
-ax2.set_title('Heart rate and  vs Duration')
+
 ax2.legend(shadow=True, fontsize='x-large', loc='lower left')
 # Show grid
 
@@ -58,5 +60,9 @@ ax3.plot(race.Power, race["Smooth heart rate"], color='green',
 ax3.set_xlabel('Power', color='blue')
 ax3.set_ylabel('Heart rate', color='red')
 ax3.set_title('Heart rate vs Power')
+
+plt.tight_layout()
+
+plt.savefig('kinomap.pdf')
 
 plt.show()
